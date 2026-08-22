442f41f73c1538fb6e023203e8f389728c45a397
utkarshgupta-ai <utkarshgupta1312004@gmail.com>
2026-08-22 18:54:44 +0530

    Fix externally-managed-environment error by adding --break-system-packages and fix distDir in vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install --break-system-packages -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
